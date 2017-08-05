5b66aa31b3053c5f2fb7387d78958728e3c4c97b
gmamar <gmamar1@hotmail.com>
2017-08-05 16:06:19 +0200

    firstStep

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      comp:[
+        {name:'george'}
+      ],
+      id:"20"
+    }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <InputFiled test= {this.state.comp[0].name} />
+          
+          <h2>Welcome to Company names</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+    );
+  }
+}
+
+class InputFiled extends Component {
+
+handleSubmit = e =>{
+  e.preventDefault()
+ console.log(e.target.value);
+  
+}
+
+  render() {
+    
+    
+    return (
+      <form >
+        Your name<br/>
+        <input type='text' onChange={e => this.handleSubmit(e)} /> 
+        
+        <button onClick={this.handleSubmit}>Submit</button>
+        <div>{this.props.test}</div>
+        {console.log(this.props.test)}
+        
+      </form>
+      
+      
     );
   }
 }
