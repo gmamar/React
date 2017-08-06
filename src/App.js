143d88d298b1a495e8e9c58d7406c9de49cd16cc
gmamar <gmamar1@hotmail.com>
2017-08-06 11:47:11 +0200

    last of the last

--- src/App.js
+++ src/App.js
@@ -7,22 +7,58 @@ class App extends Component {
     super();
     this.state={
       comp:[
-        {name:'george'}
+        {
+          name:'george',
+          company: "test"
+        },
+        {
+          name:'asdf',
+          company: "tsdfest"
+        }
       ],
-      id:"20"
+      id:"0"
     }
   }
+
+handleSubmit = e =>{
+  const current = this.state.comp.slice(1, this.state.comp.id);
+  console.log (current);
+  console.log (this.state.comp);
+  e.preventDefault()
+
+ console.log(e.target.value);
+  this.setState({name: e.target.value} )
+}
+
   render() {
+    const moves = this.state.comp.map((item, index) => {
+      
+      return (
+        <li key={index}>
+          <a href="#" >{item}</a>
+        </li>
+      );
+    });
+
     return (
       <div className="App">
         <div className="App-header">
-          <InputFiled test= {this.state.comp[0].name} />
+          <InputFiled test= {this.state.comp[0].name} onChange = { e => this.handleSubmit(e)} />
           
           <h2>Welcome to Company names</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started.
+          enter your name,the name of company.
         </p>
+        {this.state.comp.map(list =>
+            	<List
+                  id={list.id}
+                  key={list.id}
+                  title={list.name}
+                />
+            )}
+        
       </div>
     );
   }
@@ -30,28 +66,33 @@ class App extends Component {
 
 class InputFiled extends Component {
 
-handleSubmit = e =>{
-  e.preventDefault()
- console.log(e.target.value);
-  
-}
 
   render() {
     
     
     return (
       <form >
-        Your name<br/>
-        <input type='text' onChange={e => this.handleSubmit(e)} /> 
-        
+        <div>
+        <h3>Enter your data</h3>
+        </div>
+        <input type='text' onChange={this.props.onChange} />@
+        <input type='text' onChange={this.props.onchange} />
         <button onClick={this.handleSubmit}>Submit</button>
-        <div>{this.props.test}</div>
-        {console.log(this.props.test)}
+        
+        
         
       </form>
       
       
     );
+  }
+}
+
+class List extends Component {
+  render(){
+    return(
+  <h1>{this.props.title}</h1>
+    )
   }
 }
 
